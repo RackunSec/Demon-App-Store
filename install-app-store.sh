162de5a65863939ced9464a49ae7fee23a2449b8
WeakNetLabs <weaknetlabs@gmail.com>
2019-08-20 21:11:17 +0000

    Updtae the installer.

--- install-app-store.sh
+++ install-app-store.sh
@@ -3,3 +3,4 @@ cp demon-app-store.sh /usr/local/sbin/
 chmod +x /usr/local/sbin/demon-app-store.sh
 mkdir -p /usr/share/demon/images/icons 2>/dev/null
 cp icons/* /usr/share/demon/images/icons
+cp Demon\ App\ Store.desktop /root/Desktop/
