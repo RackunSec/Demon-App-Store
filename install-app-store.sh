052e6db7fa867ac98490c8a06744cca8fb1d76bb
Douglas Berdeaux <weaknetlabs@gmail.com>
2019-08-22 10:37:52 -0400

    Fixed bug

--- install-app-store.sh
+++ install-app-store.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+rm /usr/local/sbin/demon-app-store.sh 2>/dedv/null
 cp demon-app-store.sh /usr/local/sbin/
 chmod +x /usr/local/sbin/demon-app-store.sh
 mkdir -p /usr/share/demon/images/icons 2>/dev/null
