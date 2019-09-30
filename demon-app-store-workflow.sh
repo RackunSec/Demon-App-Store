a55f29690bec73bac7efcf28b9bd50951f1f98ca
Douglas Berdeaux <weaknetlabs@gmail.com>
2019-09-30 10:16:06 -0400

    EyeWitness bugs, OS!=Distro

--- demon-app-store-workflow.sh
+++ demon-app-store-workflow.sh
@@ -982,7 +982,7 @@ main () {
     --window-icon=$DAS_WINDOWICON \
     --center \
     $(if [[ $(which autosploit|wc -l) -eq 1 ]]; then printf "true"; else printf "false"; fi) "AutoSploit" "$DAS_CAT_PEN" "Automated Mass Exploit Tool" false \
-    $(if [[ $(which eyewitness|wc -l) -eq 1 ]]; then printf "true"; else printf "false"; fi) "EyeWitness" "$DAS_CAT_PEN" "Automated Web Vulnerability Tool" false \
+    $(if [[ $(which EyeWitness.sh|wc -l) -eq 1 ]]; then printf "true"; else printf "false"; fi) "EyeWitness" "$DAS_CAT_PEN" "Automated Web Vulnerability Tool" false \
     $(if [[ $(which imsi-catcher|wc -l) -eq 1 ]]; then printf "true"; else printf "false"; fi) "IMSI-Catcher" "$DAS_CAT_PEN" "IMSI Catcher Tool" false \
     $(if [[ $(which pixiewps|wc -l) -eq 1 ]]; then printf "true"; else printf "false"; fi) "PixieWPS" "$DAS_CAT_PEN" "Cracking WPS PIN" false \
     $(if [[ $(which nessus|wc -l) -eq 1 ]]; then printf "true"; else printf "false"; fi) "Nessus" "$DAS_CAT_PEN" "Tenable's vulnerability scanner" false \
