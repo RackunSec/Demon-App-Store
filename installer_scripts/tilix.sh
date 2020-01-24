53fe6357b277a824ed5c9083e2a6fa490b426378
Douglas Berdeaux <trevelyn@lab.weaknetlabs.com>
2020-01-24 20:55:49 +0000

    Added module for Tilix

---
+++ installer_scripts/tilix.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# 2020 Demon App Store
+# WeakNet Labs
+#
+# Installer script, should be called from the workflow app
+# INSTALL:
+# --------------------
+# Tilix, Tilix-Common
+# --------------------
+# NOTES:
+#   Tilix Terminal Emulator is pretty slick.
+#
+apt install -y tilix tilix-common # simple
