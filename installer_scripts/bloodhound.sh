47627df96df7b3c181beaf3a2094bfcf9188651e
Douglas Berdeaux <trevelyn@lab.weaknetlabs.com>
2020-01-26 16:14:04 +0000

    Fixed BUG with $PATH

--- installer_scripts/bloodhound.sh
+++ installer_scripts/bloodhound.sh
@@ -10,7 +10,9 @@
 # NOTES:
 #   This is a HUGE package/module.
 #
-export DAS_CONFIG=./das_config.txt # This is REQUIRED
+export DAS_LOCAL=/var/demon/store/code/Demon-App-Store/
+export DAS_CONFIG=${DAS_LOCAL}das_config.txt # This is required
+export DAS_FUNC_SCRIPT_DIR=$(cat $DAS_FUNC_SCRIPT_DIR|grep DAS_APPCACHE|sed -r 's/[^=]+=//')
 ##### ##### ##### ##### #####
 export DAS_DESKTOP_CACHE=$(cat $DAS_CONFIG|grep DAS_DESKTOP_CACHE | sed -r 's/[^=]+=//')
 export DAS_APPCACHE=$(cat $DAS_CONFIG|grep DAS_APPCACHE | sed -r 's/[^=]+=//')
