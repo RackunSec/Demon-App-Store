1ad5ffec1c40ca29691d05c736d268abb3019243
Douglas Berdeaux <trevelyn@lab.weaknetlabs.com>
2020-01-29 15:20:56 +0000

    Fixed BUG with $PATH in das_config.txt

--- installer_scripts/awus1900-driver-dkms.sh
+++ installer_scripts/awus1900-driver-dkms.sh
@@ -16,7 +16,7 @@ printf "\n[INFO] Installer script initiated: $(echo $0)\n"
 ##### Demon App Store Variables:
 export DAS_LOCAL=/var/demon/store/code/Demon-App-Store/
 export DAS_CONFIG=${DAS_LOCAL}das_config.txt # This is required
-export DAS_FUNC_SCRIPT_DIR=$(cat $DAS_FUNC_SCRIPT_DIR|grep DAS_APPCACHE|sed -r 's/[^=]+=//')
+export DAS_FUNC_SCRIPT_DIR=$(cat $DAS_CONFIG|grep DAS_FUNC_SCRIPT_DIR|sed -r 's/[^=]+=//')
 # Example of pulling variable from das_config:
 # $(cat $DAS_CONFIG|grep DAS_APPCACHE|sed -r 's/[^=]+=//')
 DAS_DL_URL=https://github.com/aircrack-ng/rtl8812au
